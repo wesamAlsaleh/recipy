2fed6ea82562d2774d6761e36537839531792f26
wesamAlsaleh <wesammuneer7@gmail.com>
2026-05-18 20:16:06 +0300

    Initial commit

---
+++ src/main/java/com/avocadogroup/recipy/RecipyApplication.java
@@ -0,0 +1,13 @@
+package com.avocadogroup.recipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avocadogroup/recipy/RecipyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocadogroup.recipy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
